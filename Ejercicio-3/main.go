ef1599265e14ce62f1d94a071fe976c4fb4153bf
Christian Alberto Tamayo Robayo <christian.tamayo@mercadolibre.com.co>
2021-12-27 09:01:51 -0500

    Se implemento la solución para el ejercicio número 3

---
+++ Ejercicio-3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var 1nombre string
+	var _nombre string
+	var apellido string
+	// var int edad
+	var edad int
+	// 1apellido := 6
+	_apellido := 6
+	var licencia_de_conducir = true
+	// var estatura de la persona int
+	var estatura_de_la_persona int
+	cantidadDeHijos := 2
+	fmt.Println(_nombre, apellido, _apellido, edad, licencia_de_conducir, estatura_de_la_persona, cantidadDeHijos)
+}
