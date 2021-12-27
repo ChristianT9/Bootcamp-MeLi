5983a40806877d87050094e5ab4e3b12c7eaa518
Christian Alberto Tamayo Robayo <christian.tamayo@mercadolibre.com.co>
2021-12-27 09:07:32 -0500

    Se implemento la solución para el ejercicio número 4

---
+++ Ejercicio-4/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var apellido string = "Gomez"
+	// var edad int = "35"
+	var edad int = 35
+	// boolean := "false";
+	test := false
+	// var sueldo string = 45857.90
+	var sueldo string = "45857.90"
+	var nombre string = "Julián"
+
+	fmt.Println(apellido, edad, test, sueldo, nombre)
+}
