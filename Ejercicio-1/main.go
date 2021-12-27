73530f433b5002d31505af63c0a90dcd541e5e52
Christian Alberto Tamayo Robayo <christian.tamayo@mercadolibre.com.co>
2021-12-26 20:30:47 -0500

    Solucion al ejercicio 1

---
+++ Ejercicio-1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombre = "Christian Alberto Tamayo Robayo"
+	var direccion = "Calle 25 #7-32"
+	fmt.Println("Nombre:", nombre)
+	fmt.Println("Dirección:", direccion)
+}
