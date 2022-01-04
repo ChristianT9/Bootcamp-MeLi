ac95df7aaad4c4782f69ebf48e15bfe0d521133e
Christian Alberto Tamayo Robayo <christian.tamayo@mercadolibre.com.co>
2022-01-04 09:48:49 -0500

    Ejercicios terminados

--- Ejercicio-1/main.go
+++ Ejercicio-1/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -23,6 +26,21 @@ func main() {
 	router.GET("/user/:name", func(ctxt *gin.Context) {
 		name := ctxt.Param("name")
 		ctxt.String(http.StatusOK, "Hola %s", name)
+	})
+
+	datosJson, err := ioutil.ReadFile("products.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	productos := Productos{}
+	err = json.Unmarshal(datosJson, &productos)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router.GET("/productos", func(ctxt *gin.Context) {
+		ctxt.JSON(http.StatusOK, productos)
 	})
 
 	router.Run(":8080")
